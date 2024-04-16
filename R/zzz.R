92f4f3a8bf57c681c66a4a54e914ce12b7986f01
alenastern <alena7690@gmail.com>
2024-04-16 10:57:32 -0400

    replace with prod zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,8 @@
 .onLoad <- function(libname, pkgname){
 
-  assign("stage", "staging", envir = parent.env(environment()))
+  assign("stage", "production", envir = parent.env(environment()))
   assign("base_url",
-         "https://equity-tool-api-stg.urban.org",
-         envir = parent.env(environment()))
-  assign("s3_bucket",
-         "ui-sedt-stg",
+         "https://equity-tool-api.urban.org",
          envir = parent.env(environment()))
 
   msg <- paste("Loading", pkgname, " - using ", stage, " API")
