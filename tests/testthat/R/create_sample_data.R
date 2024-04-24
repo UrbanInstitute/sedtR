283f151ee146950df9df6af8c50f77a6e6b011d8
deckart2 <gdmorrison@uchicago.edu>
2024-04-24 18:37:30 -0400

    start of tests that supplemental dem data is analyzed correctly

--- tests/testthat/R/create_sample_data.R
+++ tests/testthat/R/create_sample_data.R
@@ -12,10 +12,10 @@ create_sample_data <- function(){
                                                   "masters_degree" = "B15003_023",
                                                   "owner_occupied" = "B25003_002"))
 
-  write_csv(geo_county, "data/test_api_geo_county.csv")
+  write_csv(geo_county, "tests/testthat/data/test_api_geo_county.csv")
 
   # both geographic and demographic for county is at tract level, so use same data
-  write_csv(geo_county, "data/test_api_dem_county.csv")
+  write_csv(geo_county, "tests/testthat/data/test_api_dem_county.csv")
 
   # Washington State
   geo_state <- tidycensus::get_acs(geography = "county",
@@ -27,7 +27,7 @@ create_sample_data <- function(){
                                                  "masters_degree" = "B15003_023",
                                                  "owner_occupied" = "B25003_002"))
 
-  write_csv(geo_state, "data/test_api_geo_state.csv")
+  write_csv(geo_state, "tests/testthat/data/test_api_geo_state.csv")
 
   # National
   geo_national <- tidycensus::get_acs(geography = "state",
@@ -39,19 +39,23 @@ create_sample_data <- function(){
                                                     "owner_occupied" = "B25003_002")) %>%
     filter(GEOID != "72")
 
-  write_csv(geo_national, "data/test_api_geo_national.csv")
+  write_csv(geo_national, "tests/testthat/data/test_api_geo_national.csv")
 
   # Washington State
   dem_state <- tidycensus::get_acs(geography = "tract",
                                    state = "53",
                                    year = 2021,
                                    output = "wide",
-                                   variables = c("male_under_5" = "B01001_003",
-                                                 "female_under_5" = "B01001_027",
-                                                 "masters_degree" = "B15003_023",
-                                                 "owner_occupied" = "B25003_002"))
+                                   variables = c(
+                                                 "num_black" = "B03002_004",
+                                                 "num_white" = "B03002_003",
+                                                 "num_seniors" = "DP05_0024",
+                                                 "num_asian" = "DP05_0080",
+                                                 "num_children" = "DP05_0019",
+                                                 "num_hisp" = "DP05_0071"
+                                                 ))
 
-  write_csv(dem_state, "data/test_api_dem_state.csv")
+  write_csv(dem_state, "tests/testthat/data/test_api_dem_state.csv")
 
   # National
   state_codes <- tidycensus::fips_codes %>%
@@ -70,13 +74,13 @@ create_sample_data <- function(){
                                                 "masters_degree" = "B15003_023",
                                                 "owner_occupied" = "B25003_002")))
 
-  write_csv(dem_national, "data/test_api_dem_national.csv")
+  write_csv(dem_national, "tests/testthat/data/test_api_dem_national.csv")
 
   # filter mental health facilities to washington
 
   wa_mental_health <- readr::read_csv("https://equity-tool-api-stg.urban.org/sample-data/wa_mental_health.csv") %>%
     filter(state == "WA")
 
-  write_csv(wa_mental_health, "data/wa_mental_health.csv")
+  write_csv(wa_mental_health, "tests/testthat/data/wa_mental_health.csv")
 
 }
--- tests/testthat/test_everything-successful-state.R
+++ tests/testthat/test_everything-successful-state.R
@@ -8,10 +8,12 @@ test_that("API is able to call successfully for state", {
     demographic_file_path = here::here("tests", "testthat", "data", "test_api_dem_state.csv"),
     demographic_geo_id_column = "GEOID",
     demographic_columns =  list(
-      male_under_5E = "male_under_5M",
-        female_under_5E = "female_under_5M",
-        masters_degreeE = "masters_degreeM",
-        owner_occupiedE = "owner_occupiedM"
+      num_blackE = "num_blackM",
+      num_whiteE = "num_whiteM",
+      num_seniorsE = "num_seniorsM",
+      num_childrenE = "num_childrenM",
+      num_asianE = "num_asianM",
+      num_hispE = "num_hispM"
     ),
     geographic_file_path = here::here("tests", "testthat", "data", "test_api_geo_state.csv"),
     geographic_geo_id_column = "GEOID",
@@ -25,18 +27,63 @@ test_that("API is able to call successfully for state", {
 
 
   r <- get_api_response(param_list)
+  df_list <- get_api_results(r$file_id)
 
-  counter <- 0
-  sedt_status <- get_status(r$file_id)
+  geo_bias_data <- df_list$geo_bias_data
+  demo_bias_data <- df_list$demo_bias_data
 
-  while(!(isTRUE(sedt_status$results$formdata$updates$finished)) & counter < 50){
-    sedt_status <- get_status(r$file_id)
-    counter = counter + 1
-    Sys.sleep(3L)
+  #Get diff_data_city for specific portions:
+  get_diff <- function(var) {
+    val <- demo_bias_data |>
+      filter(census_var == var) |>
+      pull(diff_data_city)
+    print(val)
   }
 
-  expect_equal(sedt_status$results$formdata$updates$finished,
-               TRUE)
+
+  white_tool <- get_diff("pct_white")
+  white_sup <- get_diff("pct_num_whiteE_demographic")
+
+  black_tool <- get_diff("pct_black")
+  black_sup <- get_diff("pct_num_blackE_demographic")
+
+  asian_tool <- get_diff("pct_asian")
+  asian_sup <- get_diff("pct_num_asianE_demographic")
+
+  hisp_tool <- get_diff("pct_hisp")
+  hisp_sup <- get_diff("pct_num_hispE_demographic")
+
+  children_tool <- get_diff("pct_children")
+  children_sup <- get_diff("pct_num_childrenE_demographic")
+
+  seniors_tool <- get_diff("pct_seniors")
+  seniors_sup <- get_diff("pct_num_seniorsE_demographic")
+
+  testthat::expect_true(abs(white_tool - white_sup) < .04)
+  testthat::expect_true(abs(black_tool - black_sup) < .02)
+  testthat::expect_true(abs(asian_tool - asian_sup) < .03)
+  testthat::expect_true(abs(hisp_tool - hisp_sup) < .04)
+  testthat::expect_true(abs(children_tool - children_sup) < .05)
+  testthat::expect_true(abs(seniors_tool - seniors_sup) < .05)
+
+
+
+  #
+  #
+  # counter <- 0
+  # sedt_status <- get_status(r$file_id)
+  #
+  # while(!(isTRUE(sedt_status$results$formdata$updates$finished)) & counter < 50){
+  #   sedt_status <- get_status(r$file_id)
+  #   counter = counter + 1
+  #   Sys.sleep(3L)
+  # }
+  #
+  # expect_equal(sedt_status$results$formdata$updates$finished,
+  #              TRUE)
   }
+
+
+
  )
 
