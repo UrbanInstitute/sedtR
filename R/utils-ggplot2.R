9711f349ae98de04038baf73cf3cd888c80a43d2
Eli Pousson <eli.pousson@gmail.com>
2024-10-07 20:12:00 -0400

    Add geom_barbell
    Also add missing ggplot2 package namespacing

--- R/utils-ggplot2.R
+++ R/utils-ggplot2.R
@@ -1,5 +1,5 @@
 #' @noRd
-GeomLollipop <- ggplot2::ggproto("GeomLollipop", Geom,
+GeomLollipop <- ggplot2::ggproto("GeomLollipop", ggplot2::Geom,
 
                         required_aes = c("x", "y"),
 
@@ -16,10 +16,10 @@ GeomLollipop <- ggplot2::ggproto("GeomLollipop", Geom,
                         ),
 
                         draw_panel = function(data, panel_params, coord, ...) {
-                          # Return all three components
+                          # Return both the line and point components
                           grid::gList(
-                            GeomSegment$draw_panel(data, panel_params, coord, ...),
-                            GeomPoint$draw_panel(transform(data), panel_params, coord, ...)
+                            ggplot2::GeomSegment$draw_panel(data, panel_params, coord, ...),
+                            ggplot2::GeomPoint$draw_panel(transform(data), panel_params, coord, ...)
                           )
                         }
 )
@@ -29,10 +29,59 @@ geom_lollipop <- function(mapping = NULL, data = NULL,
                           stat = "identity", position = "identity",
                           ..., na.rm = FALSE, show.legend = NA,
                           inherit.aes = TRUE) {
-  layer(
+  ggplot2::layer(
     data = data,
     mapping = mapping,
     geom = GeomLollipop,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+#'
+#' @noRd
+GeomBarbell <- ggplot2::ggproto("GeomBarbell", ggplot2::Geom,
+
+                                 required_aes = c("x", "y", "xend|yend"),
+
+                                 default_aes = ggplot2::aes(
+                                   xend = 0,
+                                   yend = 0,
+                                   colour = "black",
+                                   # linewidth = ggplot2::from_theme(linewidth),
+                                   size = 1,
+                                   linetype = 1,
+                                   shape = 19,
+                                   fill = NA,
+                                   alpha = NA,
+                                   stroke = 1
+                                 ),
+
+                                 draw_panel = function(data, panel_params, coord, ...) {
+                                   # Transformed data for the points
+                                   point1 <- transform(data)
+                                   point2 <- transform(data, x = xend, y = yend)
+
+                                   # Return all three components
+                                   grid::gList(
+                                     ggplot2::GeomSegment$draw_panel(data, panel_params, coord, ...),
+                                     ggplot2::GeomPoint$draw_panel(point1, panel_params, coord, ...),
+                                     ggplot2::GeomPoint$draw_panel(point2, panel_params, coord, ...)
+                                   )
+                                 }
+)
+
+#' @noRd
+geom_barbell <- function(mapping = NULL, data = NULL,
+                         stat = "identity", position = "identity",
+                         ..., na.rm = FALSE, show.legend = NA,
+                         inherit.aes = TRUE) {
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    geom = GeomBarbell,
     stat = stat,
     position = position,
     show.legend = show.legend,
